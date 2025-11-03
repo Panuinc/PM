7626f141830028709c480a270652772215403c7a
Panuinc <panuwat.ja.forwork@gmail.com>
2025-11-03 11:36:25 +0700

    update

---
+++ app/(pages)/layout.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ChevronLeft } from "lucide-react";
+
+export default function PagesLayout({ children }) {
+  return (
+    <div className="flex flex-row items-center justify-center w-full h-full gap-2">
+      <div className="flex flex-row items-center justify-center w-3/12 h-full p-2 gap-2 border-2 border-dark border-dashed">
+        <div className="flex flex-col items-center justify-between w-3/12 h-full gap-2 border-2 border-dark overflow-auto">
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
+            Main
+          </div>
+          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+            Main
+          </div>
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
+            <ChevronLeft />
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-start w-9/12 h-full gap-2 border-2 border-dark overflow-auto">
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
+            Sub
+          </div>
+          <div className="flex items-center justify-center w-full h-full p-2 gap-2 border-2 border-dark border-dashed">
+            Sub
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center w-9/12 h-full p-2 gap-2 border-2 border-dark border-dashed">
+        <div className="flex flex-row items-center justify-center w-full h-fit gap-2 border-2 border-dark">
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
+            Header
+          </div>
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
+            Header
+          </div>
+          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
+            Header
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-start w-full h-full gap-2 border-2 border-dark overflow-auto">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/home/UIHome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function UIHome() {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
+        1
+      </div>
+    </>
+  );
+}
