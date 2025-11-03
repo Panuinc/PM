386876a605d1a4838977cca9d1570e7d5144341c
Panuinc <panuwat.ja.forwork@gmail.com>
2025-11-03 12:05:35 +0700

    update

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -6,7 +6,7 @@ export default function PagesLayout({ children }) {
   return (
     <div className="flex flex-row items-center justify-center w-full h-full gap-2">
       <div className="flex flex-row items-center justify-center w-3/12 h-full p-2 gap-2 border-2 border-dark border-dashed">
-        <div className="flex flex-col items-center justify-between w-3/12 h-full gap-2 border-2 border-dark overflow-auto">
+        <div className="flex flex-col items-center justify-between w-3/12 h-full p-2 gap-2 border-2 border-dark overflow-auto">
           <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
             Main
           </div>
@@ -17,7 +17,7 @@ export default function PagesLayout({ children }) {
             <ChevronLeft />
           </div>
         </div>
-        <div className="flex flex-col items-center justify-start w-9/12 h-full gap-2 border-2 border-dark overflow-auto">
+        <div className="flex flex-col items-center justify-start w-9/12 h-full p-2 gap-2 border-2 border-dark overflow-auto">
           <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
             Sub
           </div>
@@ -27,9 +27,9 @@ export default function PagesLayout({ children }) {
         </div>
       </div>
       <div className="flex flex-col items-center justify-center w-9/12 h-full p-2 gap-2 border-2 border-dark border-dashed">
-        <div className="flex flex-row items-center justify-center w-full h-fit gap-2 border-2 border-dark">
-          <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
-            Header
+        <div className="flex flex-row items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark">
+          <div className="flex items-center justify-center w-fit h-fit p-2 gap-2 border-2 border-dark border-dashed whitespace-nowrap">
+            Preventive Maintenance
           </div>
           <div className="flex items-center justify-center w-full h-fit p-2 gap-2 border-2 border-dark border-dashed">
             Header
@@ -38,7 +38,7 @@ export default function PagesLayout({ children }) {
             Header
           </div>
         </div>
-        <div className="flex flex-col items-center justify-start w-full h-full gap-2 border-2 border-dark overflow-auto">
+        <div className="flex flex-col items-center justify-start w-full h-full p-2 gap-2 border-2 border-dark overflow-auto">
           {children}
         </div>
       </div>
