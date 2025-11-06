bee5bc5e14f1635bec3bb3acc0c3418589382851
Panuinc <panuwat.ja.forwork@gmail.com>
2025-11-06 13:40:07 +0700

    update

---
+++ app/(pages)/setting/user/create/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+import UIUserForm from "@/components/setting/user/UIUserForm";
+import { useSessionUser } from "@/hooks/useSessionUser";
+import { useSubmitUser } from "@/app/api/setting/user/hooks";
+import { useFormHandler } from "@/hooks/useFormHandler";
+
+export default function UserCreate() {
+  const { userId, userName } = useSessionUser();
+  const submitUser = useSubmitUser({ mode: "create", userId });
+
+  const formHandler = useFormHandler(
+    {
+      userName: "",
+    },
+    submitUser
+  );
+
+  return (
+    <>
+      <UIUserForm
+        headerTopic="User Create"
+        formHandler={formHandler}
+        mode="create"
+        operatedBy={userName}
+      />
+    </>
+  );
+}
