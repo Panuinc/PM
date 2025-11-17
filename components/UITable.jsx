acc063784ddb8cbccadadce07317ad2bb5714438
Panuinc <panuwat.ja.forwork@gmail.com>
2025-11-17 09:11:00 +0700

    updateTable

--- components/UITable.jsx
+++ components/UITable.jsx
@@ -267,6 +267,7 @@ export default function DataTable({
             <option value="5">5</option>
             <option value="10">10</option>
             <option value="15">15</option>
+            <option value="50">50</option>
           </select>
         </label>
       </div>
